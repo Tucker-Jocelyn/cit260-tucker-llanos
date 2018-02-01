99648af390f6a47b40312bcedf5ee714f73e651e
kimta <kimta@Kimmie-ASUS-Q534UX>
2018-01-31 20:47:12 -0800

    1-31-18 re-did Player Class within the (new) OregonTrail.model package. Noticed changes in the import statements after this was done. Ran, but msg indicated there were errors - even though a "build sucessful" msg displayed after running. We need to investigate whatever errors msg was referring to.

---
+++ oregonTrail/src/OregonTrail/model/AnimalsPlants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OregonTrail.model;
+
+/**
+ *
+ * @author kimta
+ */
+public enum AnimalsPlants {
+    
+    private int animalWeight;
+    private int plantWeight;
+
+    
+}
