9d84df2fc29cb061086d41be0dc86e95eac6f640
kimta <kimta@Kimmie-ASUS-Q534UX>
2018-01-31 20:43:36 -0800

    1-31-18 re-did Player Class within the (new) OregonTrail.model package. Noticed changes in the import statements after this was done. Ran, but msg indicated there were errors - even though a "build sucessful" msg displayed after running. We need to investigate whatever errors msg was referring to.

---
+++ oregonTrail/src/OregonTrail/model/Player.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OregonTrail.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+/**
+ *
+ * @author kimta
+ */
+public class Player implements Serializable{
+    
+    //class instance variables
+    private String name;
+    private ArrayList<Game> games = new ArrayList<>();
+
+    public Player() {
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        
+    }
+
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;      
+        
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.games);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", games=" + games + '}';
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.games, other.games)) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
+    
