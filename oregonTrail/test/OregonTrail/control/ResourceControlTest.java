b21e4e75d448715654c694600f42f745d32c1f06
Jocelyn <Jocelyn@192.168.0.15>
2018-02-15 09:16:58 -0800

    2/15/18
    We set up our function in the wrong class last time. Moved from CharacterControl to ResourceControl. Also moved the tests from CharacterControl to ResourceControl.
    
    Changed the author name in each of the classes we made last time. (It puts in my husband's name because I use his computer. JT)

--- oregonTrail/test/OregonTrail/control/ResourceControlTest.java
+++ oregonTrail/test/OregonTrail/control/ResourceControlTest.java
@@ -12,11 +12,11 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Tim
+ * @author Jocelyn Tucker & Kim Llanos
  */
-public class CharacterControlTest {
+public class ResourceControlTest {
     
-    public CharacterControlTest() {
+    public ResourceControlTest() {
     }
     
     @BeforeClass
@@ -28,8 +28,8 @@ public class CharacterControlTest {
     }
 
     /**
-     * Test of calcWagonLoadWeight method, of class CharacterControl.
-    *test 1 valid wagon load weight + animal & plant weights
+    * Test of calcWagonLoadWeight method, of class ResourceControl.
+    * test 1 valid wagon load weight + animal & plant weights
     * test 2 invalid wagon load weight + animal & plant weights
     * test 3 upper boundary wagon load weight + animal & plant weights
     * test 4 lower boundary wagon load weight + animal & plant weights
@@ -52,12 +52,11 @@ public class CharacterControlTest {
         double expResult = 3075;
         
         //call the method
-        double result = CharacterControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
+        double result = ResourceControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
         
         //test to see if the result returned equals the expected result
         assertEquals(expResult, result, 0.0001);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+       
    
     
 //test 2 (columns 3 & 4 of test matrix)Invalid parameters wagon load weight + team animal plant weight > max load weight
@@ -74,12 +73,11 @@ public class CharacterControlTest {
         expResult = -999;
         
         //call the method
-        result = CharacterControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
+        result = ResourceControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
         
         //test to see if the result returned equals the expected result
         assertEquals(expResult, result, 0.0001);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+       
         
        //test 3 (boundary columns 9 & 10 upper limit in test matrix)Upper wagon load weight boundary = valid with added animals and plants
         System.out.println("calcWagonLoadWeight");
@@ -95,12 +93,11 @@ public class CharacterControlTest {
         expResult = 4000;
         
         //call the method
-        result = CharacterControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
+        result = ResourceControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
         
         //test to see if the result returned equals the expected result
         assertEquals(expResult, result, 0.0001);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        
         
         //test 4 (boundary columns 11 & 12 lower limit in test matrix)lower wagon load weight boundary = valid with added animals and plants
         System.out.println("calcWagonLoadWeight");
@@ -116,11 +113,10 @@ public class CharacterControlTest {
         expResult = 1375;
         
         //call the method
-        result = CharacterControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
+        result = ResourceControl.calcWagonLoadWeight(teamAnimalWeight, teamPlantWeight, currentWagonLoadWeight, maxCarryWeight);
         
         //test to see if the result returned equals the expected result
         assertEquals(expResult, result, 0.0001);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        
 }
 }
