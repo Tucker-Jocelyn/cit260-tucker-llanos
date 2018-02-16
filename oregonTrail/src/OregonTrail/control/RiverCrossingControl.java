0ab8ec4fb8274d53e672a265a2ab15bfbab0c718
Jocelyn <Jocelyn@192.168.0.15>
2018-02-16 12:03:08 -0800

    2/16/18
    Added some comments to explain code in calcSuccessProbability function.
    
    Created all test cases from test matrix.
    
    All tests passed.

--- oregonTrail/src/OregonTrail/control/RiverCrossingControl.java
+++ oregonTrail/src/OregonTrail/control/RiverCrossingControl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package OregonTrail.control;
 
 /**
@@ -11,7 +6,7 @@ package OregonTrail.control;
  */
 public class RiverCrossingControl {
     
-public static double calcSuccessProbability(double currentWagonLoadWeight, double wagonWeight, double waterDepth, int length, int oxHealth) {
+    public static double calcSuccessProbability(double currentWagonLoadWeight, double wagonWeight, double waterDepth, int length, int oxHealth) {
 
 	if (currentWagonLoadWeight < 0 || wagonWeight < 1250 || wagonWeight > 1350 || waterDepth < 1 || 
             waterDepth > 4.75 || length < 9 || length > 11 || oxHealth < 0) {
@@ -19,7 +14,7 @@ public static double calcSuccessProbability(double currentWagonLoadWeight, doubl
 	}
 	
         double percentSuccess = 0;
-	double totalWagonWeight = currentWagonLoadWeight + wagonWeight;
+	double totalWagonWeight = currentWagonLoadWeight + wagonWeight; //pushing the wagon down
 	
 	if (waterDepth * 12 < 40) {  //convert water depth in feet to inches; wagon clearance is 40"
             percentSuccess = 100;
@@ -27,17 +22,17 @@ public static double calcSuccessProbability(double currentWagonLoadWeight, doubl
         }
 
         double netWaterDepth = ((waterDepth * 12) - 40)/12;  //depth in inches of water above 40" clearance converted to feet
-	double buoyancy = (length * 4) * netWaterDepth * 62.4;  //4 is width of wagon; 62.4 is weight of water in lbs/cubic foot (displaced water)
+	double buoyancy = (length * 4) * netWaterDepth * 62.4;  //4 is width of wagon; 62.4 is weight of water in lbs/cubic foot (displaced water) - force pushing the wagon up
 		
 	if (buoyancy >= totalWagonWeight) {  //wagon floats away
             percentSuccess = 0;
             return percentSuccess;
         }
         
-        double frictionForceCoefficient = 0;
+        double frictionForceCoefficient = 0;  //friction force coefficient is friction of wheels & oxen hooves on river bottom
         
 	if (oxHealth >= 75) {
-            frictionForceCoefficient = .4;  //friction force coefficient is friction of wheels & oxen hooves on river bottom
+            frictionForceCoefficient = .4;
         }
         
         if (oxHealth >= 50 && oxHealth < 75) {
@@ -48,8 +43,8 @@ public static double calcSuccessProbability(double currentWagonLoadWeight, doubl
             frictionForceCoefficient = .2;
 	}
 		
-	double frictionForce = frictionForceCoefficient * (totalWagonWeight - buoyancy);  
-	double waterForce = (length * netWaterDepth) * 1.4 * 36;  //1.4 is constant for square-cornered objects; 36 is velocity squared (set at 6 ft/sec because it wasn't specified in end user stories)
+	double frictionForce = frictionForceCoefficient * (totalWagonWeight - buoyancy);  //force keeping the wagon in place
+	double waterForce = (length * netWaterDepth) * 1.4 * 36;  //1.4 is constant for square-cornered objects; 36 is velocity squared (set at 6 ft/sec because it wasn't specified in end user stories) - force pushin against the side of the wagon
 	
 	if (frictionForce > waterForce) {
             percentSuccess = 100;
