1b2d69911c276b3e5d3073df4fbd4e455c1fd1df
kimta <kimta@Kimmie-ASUS-Q534UX>
2018-02-27 17:21:04 -0800

    2-27-18 Created TeamPAceMenuView and added a Menu Item (Set Team Pace) to the Game Play Menu and tested.  KL

---
+++ test/OregonTrail/view/TeamPaceMenuViewTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OregonTrail.view;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kim Tanton
+ */
+public class TeamPaceMenuViewTest {
+    
+    public TeamPaceMenuViewTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+
+    }
+    
+}
